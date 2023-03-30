fe10705aa5e2333e34a3e736fa9170e0a39bb6b8
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2023-03-30 21:55:28 +0530

    Day3:Assignment

---
+++ week-1/duplicate_debug.java
@@ -0,0 +1,28 @@
+class duplicate_debug
+{
+  static int removeDuplicates(int arr[], int n)
+  {
+    if (n==0 || n==1)
+      return n;
+      
+    int[] temp = new int[n];      
+    int j = 0;
+    for (int i = 0; i<n-1; i++)    // was:for(int j = 0; i<n-1;i++)
+      if (arr[i] != arr[i+1])
+        temp[j++] = arr[i];
+         
+    temp[j++] = arr[n-1];  
+
+    for (int i=0; i<j; i++)
+      arr[i] = temp[i];  
+    return j;
+  }
+  public static void main (String[] args)
+  {
+    int arr[] = {10, 20, 20, 30, 40, 40, 40, 50, 50}; //was: it arr[]....
+    int n = arr.length;      
+    n = removeDuplicates(arr,n); // was: n = removeDuplicates(arr)
+    for (int i=0; i<n; i++)
+      System.out.print(arr[i]+" ");
+  }
+}
