47162027c5a87dd088677a210a5c18b72e03ee9c
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2023-05-02 08:41:35 +0530

    Day-1 test

---
+++ week-2/tests/test1_1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+
+public class test1_1
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int[] arr = {14,16,87,36,25,89,34}; //Input Array
+    Arrays.sort(arr);
+    System.out.print("Enter the Mth Maximum: ");
+    int m = sc.nextInt();
+    System.out.print("Enter the Nth Minimum: ");
+    int n = sc.nextInt();
+    int sum = arr[n-1] + arr[(arr.length)-m];
+    int dif = arr[(arr.length)-m] - arr[n-1];
+    System.out.println("Mth maximum: "+arr[(arr.length)-m]+"\nNth Minimum: "+arr[n-1]);
+    System.out.println("Sum: "+sum+"\nDifference: "+dif);
+  }
+}
+
---
+++ week-2/tests/test2_1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+
+public class test2_1
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int arr1[] = new int[4];
+    int arr2[] = new int[4];
+    int bal=0;
+    for(int i = 0 ; i < 4 ; ++i)
+    {
+      System.out.print("Enter "+(i+1)+" Denomination: ");
+      arr1[i] = sc.nextInt();
+      System.out.print("Enter "+(i+1)+" Denominationno of notes: ");
+      arr2[i] = sc.nextInt();
+    }
+    for(int i = 0 ; i < 4 ; ++i)
+    {
+      bal += arr1[i]*arr2[i];
+    }
+    System.out.println("Balance: "+bal);
+  }
+}
