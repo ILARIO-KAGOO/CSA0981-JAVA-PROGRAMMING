3c8d147ddb2ae935084cc69a64bda5df21140a8a
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2023-05-02 08:59:09 +0530

    Day-1 Test

--- week-2/tests/test1_1.java
+++ week-2/tests/test1_1.java
@@ -11,10 +11,18 @@ public class test1_1
     System.out.print("Enter the Mth Maximum: ");
     int m = sc.nextInt();
     System.out.print("Enter the Nth Minimum: ");
-    int n = sc.nextInt();
-    int sum = arr[n-1] + arr[(arr.length)-m];
-    int dif = arr[(arr.length)-m] - arr[n-1];
-    System.out.println("Mth maximum: "+arr[(arr.length)-m]+"\nNth Minimum: "+arr[n-1]);
+    int n = sc.nextInt(),dn,dm;
+    if(n==0)
+    { dn = 0;}
+    else
+    { dn = arr[n-1];}
+    if(m==0)
+    { dm=0;}
+    else
+    { dm =arr[(arr.length)-m]; }
+    int sum = dn + dm;
+    int dif = dm - dn;
+    System.out.println("Mth maximum: "+dm+"\nNth Minimum: "+dn);
     System.out.println("Sum: "+sum+"\nDifference: "+dif);
   }
 }
